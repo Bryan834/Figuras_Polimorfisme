0db707189d248a1a019fe32c09e28a71da59dbcc
bryan834 <bryan.garcia@estudiantat.upc.edu>
2023-09-12 13:44:08 +0200

    clase triangulo implementada

--- src/GestorFiguras.java
+++ src/GestorFiguras.java
@@ -20,9 +20,10 @@ public class GestorFiguras {
     }
 
     public static void main(String[] args) {
-        Figura[] v = new Figura[2];
+        Figura[] v = new Figura[3];
         v[0] = new Circulo(5);
         v[1] = new Rectangulo(2,1);
+        v[2] = new Triangulo(2,3);
 
         double r = GestorFiguras.suma(v);
 
