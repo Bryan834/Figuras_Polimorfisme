522ea1835516da3c29c03143124d1d6c4a46deb6
bryan834 <bryan.garcia@estudiantat.upc.edu>
2023-09-14 10:20:35 +0200

    clase cuadrado implementada

--- src/figures/Cuadrado.java
+++ src/figures/Cuadrado.java
@@ -1,4 +1,19 @@
 package figures;
 
-public class Cuadrado {
+public class Cuadrado extends Rectangulo{
+    public Cuadrado(){
+    }
+    private double l1;
+    public Cuadrado (double l1) {
+        this.l1 = l1;
+    }
+    public double area() {
+        return (this.l1 * this.l1);
+    }
+    public double getL1() {
+        return l1;
+    }
+
+    public String toString() {return "El cuadrado de " + this.l1 + " de lado, tiene como área: " + this.area();}
+
 }
