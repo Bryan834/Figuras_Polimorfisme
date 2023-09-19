dfe6c0983e14f6852aa59d80e3b70044446c28b1
bryan834 <bryan.garcia@estudiantat.upc.edu>
2023-09-19 11:42:54 +0200

    clase cuadrado implementada con super

--- src/figures/Cuadrado.java
+++ src/figures/Cuadrado.java
@@ -3,17 +3,11 @@ package figures;
 public class Cuadrado extends Rectangulo{
     public Cuadrado(){
     }
-    private double l1;
+
     public Cuadrado (double l1) {
-        this.l1 = l1;
-    }
-    public double area() {
-        return (this.l1 * this.l1);
-    }
-    public double getL1() {
-        return l1;
+        super(l1,l1);
     }
 
-    public String toString() {return "El cuadrado de " + this.l1 + " de lado, tiene como área: " + this.area();}
+    public String toString() {return "El cuadrado de " + super.getL1() + " de lado, tiene como área: " + this.area();}
 
 }
